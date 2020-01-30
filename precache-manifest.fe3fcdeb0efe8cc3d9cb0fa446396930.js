f26833c55555a451cd866b0b451b14e10bac5673
blogimane <blogimane.g@gmail.com>
2020-01-30 11:59:16 +0000

    Updates

---
+++ precache-manifest.fe3fcdeb0efe8cc3d9cb0fa446396930.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5049f882361e51235d3e79df2a035648",
+    "url": "/friendslist/index.html"
+  },
+  {
+    "revision": "6e8b948c407b343ceb4e",
+    "url": "/friendslist/static/css/main.34c5d28e.chunk.css"
+  },
+  {
+    "revision": "a3d715dbc8d8d9b2334c",
+    "url": "/friendslist/static/js/2.9e70c252.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/friendslist/static/js/2.9e70c252.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6e8b948c407b343ceb4e",
+    "url": "/friendslist/static/js/main.56117315.chunk.js"
+  },
+  {
+    "revision": "3de24d326936c8736c4b",
+    "url": "/friendslist/static/js/runtime-main.79428933.js"
+  }
+]);
